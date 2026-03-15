c6f6e6dd6f8bde2bab42785091cd07bdd35a3b19
Koraxe <finnparker2007@gmail.com>
2026-03-15 20:22:05 +0000

    the start i guess

--- src/main/java/net/Koraxe/ModExperimentation/TutorialMod.java
+++ src/main/java/net/Koraxe/ModExperimentation/TutorialMod.java
@@ -1,7 +1,10 @@
 package net.Koraxe.ModExperimentation;
 
 import com.mojang.logging.LogUtils;
+import net.Koraxe.ModExperimentation.item.Moditems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -32,7 +35,7 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        Moditems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -46,7 +49,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(Moditems.ALEXANDRITE);
+            event.accept((Moditems.ALEXANDRITE_INGOT));
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/Koraxe/ModExperimentation/item/Moditems.java
+++ src/main/java/net/Koraxe/ModExperimentation/item/Moditems.java
@@ -1,4 +1,21 @@
 package net.Koraxe.ModExperimentation.item;
 
+import net.Koraxe.ModExperimentation.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",() -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ALEXANDRITE_INGOT = ITEMS.register("alexandrite_ingot", ()-> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
